d91c666f6064ef4aaee2242c3e081a6433727c80
Seyoung <seyoung2k@naver.com>
2021-11-07 01:15:27 -0800

    Made foundation of map

--- src/Map.java
+++ src/Map.java
@@ -1,6 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+
 public class Map {
-    public static void makeMap() {
-        return ;
+    JFrame frame = new JFrame("Hexxagon");
+    public void Board() {
+        frame.setVisible(true);
+        frame.setSize(1280, 828);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    public void test() {}
+    public void CreateMap() {
+        JButton mapArr [][] = new JButton[6][6];
+        JPanel pl = new JPanel();
+        pl.setBackground(Color.DARK_GRAY);
+        pl.setLayout((new GridLayout(6,7)));
+
+        for(int i = 0; i < mapArr.length; i++) {
+            for(int j = 0; j < mapArr[i].length; j++) {
+                JButton temp = new JButton(i + " " + j);
+                pl.add(temp);
+                mapArr[i][j] = temp;
+            }
+        }
+        frame.add(pl);
+    }
+
 }
