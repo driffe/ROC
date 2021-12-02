f9ef9881fb01ccb68049937888d215633aa524bf
SeyoungOh <33598679+driffe@users.noreply.github.com>
2021-12-01 20:39:15 -0800

    Update Omok.java

--- src/NewGame/Omok.java
+++ src/NewGame/Omok.java
@@ -10,6 +10,7 @@ public class Omok implements OmokInterface{
     static int outputY = 0;
     static int turn = 1;
 
+    //Give turn to players
     public int playerTurn(int turn) {
         if(turn % 2 == 1) {
             return 1;
@@ -17,12 +18,14 @@ public class Omok implements OmokInterface{
             return 2;
         }
     }
+    //Check winner if the player satisfied all winning condition.
     public int checkWinner(int[][]map) {
         int check = playerTurn(turn);
         int _x = 0;
         int _y = 0;
         int count = 0;
-        // 가로 체크 코드
+
+        //check row
         _x = outputX;
         _y = outputY;
         count = 0;
@@ -37,11 +40,12 @@ public class Omok implements OmokInterface{
         } catch (ArrayIndexOutOfBoundsException e) {
             return 0;
         }
+        //If there are five same color on map, that player is winner
         if(count == 5) {
             return 1;
         }
 
-        // 세로 체크 코드
+        // check column
         _x = outputX;
         _y = outputY;
         count = 0;
@@ -70,18 +74,17 @@ public class Omok implements OmokInterface{
             }
             while(map[++_y][++_x] == check && _y <= 10 && _x <= 10) {
                 count++;
-                // System.out.println(count+"오목체크");
             }
         } catch (ArrayIndexOutOfBoundsException e) {
             return 0;
         }
-
+        //If there are five same color on map, that player is winner
         if(count == 5) {
-            //System.out.println(name + "승리");
+
             return 1;
         }
 
-        // 대각선 ↗
+        // Check diagonal↗
         _x = outputX;
         _y = outputY;
         count = 0;
@@ -91,25 +94,25 @@ public class Omok implements OmokInterface{
                 _x--;
             }
             while(map[--_y < 0 ? 0 : _y][++_x] == check && _y <= 10 && _x <= 10) {
-                // omok[][] 에서 y 값에 조건문을 넣은 이유는 a, 1 을 입력시
-                // y 값이 -1 로 음수값으로 넘어가면 오류를 출력해서 조정해 주었습니다.
                 count++;
             }
         } catch (ArrayIndexOutOfBoundsException e) {
             return 0;
         }
-
+        //If there are five same color on map, that player is winner
         if(count == 5) {
             return 1;
         }
 
         return 0;
     }
+    //Make frame with width 1280, height 828
     public void board(JFrame frame) {
         frame.setVisible(true);
         frame.setSize(1280, 828);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+    //Map for omakMap and able to click for change color.
     public void omakMap(JButton[][] mapArr, JFrame frame, int[][] map) {
         JPanel pl = new JPanel();
         pl.setBackground(Color.DARK_GRAY);
@@ -186,6 +189,7 @@ public class Omok implements OmokInterface{
         }
         frame.add(pl);
     }
+    //When button is clicked, change it with red(player1) color
     public void paintRed(JButton[][] mapArr, int i, int j) {
         JButton temp = mapArr[i][j];
         temp.setBackground(Color.red);
@@ -194,6 +198,7 @@ public class Omok implements OmokInterface{
         temp.setFocusPainted(false);
         temp.setBorderPainted(false);
     }
+    //When button is clicked, change it with yellow(player2) color
     public void paintYellow(JButton[][] mapArr, int i, int j) {
         JButton temp = mapArr[i][j];
         temp.setBackground(Color.yellow);
